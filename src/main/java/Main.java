849cdf83ea660223e776d88cbd14d7e53b090668
smuzoo <367449@edu.itmo.ru>
2023-04-15 21:30:18 +0300

    Vehicle2.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import collection.VehicleCollenction;
+import org.jetbrains.annotations.NotNull;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String @NotNull [] args) {
         //valid на filepath
         final String FILEPATH = args[0];
         VehicleCollenction.read(FILEPATH);
 
+
     }
+
 }
--- src/main/java/collection/VehicleCollenction.java
+++ src/main/java/collection/VehicleCollenction.java
@@ -3,17 +3,19 @@ package collection;
 import Parsers.ParserFromFileToCollection;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class VehicleCollenction {
-    private static Map<UUID, Vehicle> vehicleHashMapCollection = new HashMap<>();
+    public static Map<UUID, Vehicle> vehicleHashMapCollection = new LinkedHashMap<>();
 
 
 
     public static void read(String FILE_PATH){
 
         vehicleHashMapCollection = ParserFromFileToCollection.read(FILE_PATH);
+        //System.out.println(vehicleHashMapCollection);
     }
 }
 
