59c269815c2c4d0551bab668d1157ea65df5186f
smuzoo <lalun.75@gmail.com>
2023-06-06 22:06:17 +0300

    ReplaceIfs

--- src/main/java/commands/concreteCommands/ReplaceIfGreater.java
+++ src/main/java/commands/concreteCommands/ReplaceIfGreater.java
@@ -35,20 +35,14 @@ public class ReplaceIfGreater implements Command {
                     .ifPresent(entry -> {
                         Vehicle currentVehicle = entry.getValue();
 
-                        // Обновление данных текущего объекта, кроме поля id
                         currentVehicle.setKey(entry.getKey());
                         currentVehicle.setUUID(UUID.randomUUID());
                         System.out.println("Заменен объект со значением " + currentVehicle);
 
-                        // Обновление данных текущего объекта в базе данных
                         Database dataBase = Database.getInstance();
                         int update = dataBase.updateVehicleInDatabase("vehicles", currentVehicle);
 
-                        if (update > 0) {
-                            System.out.println("Данные объекта обновлены в базе данных");
-                        } else {
-                            System.out.println("Не удалось обновить данные объекта в базе данных");
-                        }
+                        System.out.println(update > 0 ? "Данные объекта обновлены" : "Не удалось обновить данные объекта в базе данных");
                     });
 
             if (!VehicleCollection.getVehicleCollection().containsKey(argument)) {
@@ -56,6 +50,7 @@ public class ReplaceIfGreater implements Command {
             }
         }
     }
+
 
 
 
--- src/main/java/commands/concreteCommands/ReplaceIfLower.java
+++ src/main/java/commands/concreteCommands/ReplaceIfLower.java
@@ -4,7 +4,9 @@ import collection.Vehicle;
 import collection.VehicleCollection;
 import collection.edit.VehicleCreate;
 import commands.Command;
+import dataBase.Database;
 import parsers.Parsing;
+import validation.commands.RemoveElementValidatorKey;
 
 import java.util.UUID;
 
@@ -12,33 +14,39 @@ import java.util.UUID;
  * The type Replace if lower.
  */
 public class ReplaceIfLower implements Command {
-
     private final Parsing parsing;
 
-    /**
-     * Instantiates a new Replace if lower.
-     *
-     * @param parsing the parsing
-     */
     public ReplaceIfLower(Parsing parsing) {
         this.parsing = parsing;
     }
 
     @Override
     public void execute(String argument) {
-        VehicleCreate vehicleCreate = new VehicleCreate(parsing);
-        Vehicle vehicle = vehicleCreate.create();
+        RemoveElementValidatorKey removeElementValidatorKey = new RemoveElementValidatorKey(argument);
+        if (removeElementValidatorKey.isValid()) {
+            VehicleCreate vehicleCreate = new VehicleCreate(parsing);
+            Vehicle vehicle = vehicleCreate.create();
 
-        try {
-            if (VehicleCollection.getVehicleCollection().get(argument).getEnginePower() > vehicle.getEnginePower()) {
-                RemoveKey removeKey = new RemoveKey();
-                removeKey.execute(argument);
-                vehicle.setKey(argument);
-                vehicle.setUUID(UUID.randomUUID());
-                VehicleCollection.add(argument, vehicle);
+            VehicleCollection.getVehicleCollection().entrySet().stream()
+                    .filter(entry -> entry.getKey().equals(argument))
+                    .filter(entry -> entry.getValue().getEnginePower() > vehicle.getEnginePower())
+                    .findFirst()
+                    .ifPresent(entry -> {
+                        Vehicle currentVehicle = entry.getValue();
+
+                        currentVehicle.setKey(entry.getKey());
+                        currentVehicle.setUUID(UUID.randomUUID());
+                        System.out.println("Заменен объект со значением " + currentVehicle);
+
+                        Database dataBase = Database.getInstance();
+                        int update = dataBase.updateVehicleInDatabase("vehicles", currentVehicle);
+
+                        System.out.println(update > 0 ? "Данные объекта обновлены" : "Не удалось обновить данные объекта в базе данных");
+                    });
+
+            if (!VehicleCollection.getVehicleCollection().containsKey(argument)) {
+                System.out.println("Объект не заменен, так как старый меньше");
             }
-        } catch (NullPointerException e) {
-            System.out.println("У одного из сравниваемых объектов сила двигателя null, их невозможно сравнить");
         }
     }
 
